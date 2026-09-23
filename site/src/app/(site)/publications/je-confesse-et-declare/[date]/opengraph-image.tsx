70b735688fd0d0bbe20be3bfe5ebdf9473ab742b
cnkoma-web <cnkoma@gmail.com>
2026-09-23 11:35:54 -0700

    fix: generate Open Graph images dynamically

--- site/src/app/(site)/publications/je-confesse-et-declare/[date]/opengraph-image.tsx
+++ site/src/app/(site)/publications/je-confesse-et-declare/[date]/opengraph-image.tsx
@@ -1,6 +1,7 @@
 import { getPublishedArticles } from "@/lib/content/articles";
 import { renderOgImage, OG_SIZE, OG_CONTENT_TYPE } from "@/lib/og";
 
+export const dynamic = "force-dynamic";
 export const size = OG_SIZE;
 export const contentType = OG_CONTENT_TYPE;
 export const alt = "Je Confesse";
