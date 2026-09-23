d234eaed9c95d87b4e5d3c1697bdf34675754123
cnkoma-web <cnkoma@gmail.com>
2026-09-23 11:35:57 -0700

    fix: generate Open Graph images dynamically

--- site/src/app/(site)/rosee-matinale/opengraph-image.tsx
+++ site/src/app/(site)/rosee-matinale/opengraph-image.tsx
@@ -1,6 +1,7 @@
 import { getRoseeDuJour } from "@/lib/content/articles";
 import { renderOgImage, OG_SIZE, OG_CONTENT_TYPE } from "@/lib/og";
 
+export const dynamic = "force-dynamic";
 export const size = OG_SIZE;
 export const contentType = OG_CONTENT_TYPE;
 export const alt = "Rosée Matinale";
