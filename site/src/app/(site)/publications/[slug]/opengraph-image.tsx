579859335b83b3c3fdd0a29e9ac8f8c49b76cb8d
cnkoma-web <cnkoma@gmail.com>
2026-09-23 11:35:52 -0700

    fix: generate Open Graph images dynamically

--- site/src/app/(site)/publications/[slug]/opengraph-image.tsx
+++ site/src/app/(site)/publications/[slug]/opengraph-image.tsx
@@ -1,6 +1,7 @@
 import { getArticleBySlugAnyType } from "@/lib/content/articles";
 import { renderOgImage, OG_SIZE, OG_CONTENT_TYPE } from "@/lib/og";
 
+export const dynamic = "force-dynamic";
 export const size = OG_SIZE;
 export const contentType = OG_CONTENT_TYPE;
 export const alt = "Publication";
